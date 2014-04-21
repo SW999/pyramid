51e91bd36a8109b27c943cdd066dce2a8b208bd5
Siarhei Vaitehovich <swarog2002@mail.ru>
2014-04-21 16:55:13 +0300

    Added compass

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-
-# gem "rails"
-gem 'sass', "3.2.13"
-gem 'sass-globbing', ">= 1.1.0"
-gem 'compass', "0.12.2"
-gem 'breakpoint',  "2.0.7"
-gem 'singularitygs', "< 2.0.0"
-gem 'toolkit', "1.0.0"
--- config.rb
+++ config.rb
@@ -7,19 +7,26 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+#line_comments = true
+
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :development
+#environment = :production
+
+# In development, we can turn on the debug_info to use with FireSass or Chrome Web Inspector.
+debug = false
+#debug = true
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
+Sass::Plugin.options[:debug_info] = true
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the debug config variable above is true.
+sass_options = (environment == :development && debug == true) ? {:debug_info => true} : {}
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-line_comments = true
-output_style = :expanded
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Add support for repeating gradients
+Compass::BrowserSupport.add_support("repeating-linear-gradient", "webkit", "moz", "ms")
