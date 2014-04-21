f2d881e801dbd6336bc64b6b49af998b3fc37c22
Siarhei Vaitehovich <swarog2002@mail.ru>
2014-04-21 18:30:48 +0300

    Added sass variables and mixins

--- config.rb
+++ config.rb
@@ -8,16 +8,16 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-#line_comments = true
+#line_comments = false
+line_comments = true
 
 # Change this to :production when ready to deploy the CSS to the live server.
 environment = :development
 #environment = :production
 
 # In development, we can turn on the debug_info to use with FireSass or Chrome Web Inspector.
-debug = false
-#debug = true
+#debug = false
+debug = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
